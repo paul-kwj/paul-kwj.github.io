f36605e67cb4118ea25f1bca539671d9d1d59248
Paulkwj <paulkwj@gmail.com>
2021-06-04 20:45:42 -0400

    jquery

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+
+// Then bind the event to the callback function.
+// There are several events for cross browser compatibility.
+script.onreadystatechange = handler;
+script.onload = handler;
+
+// Fire the loading
+head.appendChild(script);
+
+function handler(){
+   console.log('jquery added :)');
+}
+
 $(document).ready(function() {
 	var start = 0;
 	$("#next").click(function(){
